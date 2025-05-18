56956604a7bc1162ad64ef7fdfa4d7e162d06857
RaflyAdiyasa <rafly271008@gmail.com>
2025-05-18 17:16:29 +0700

    add -page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import NoteList from "./components/NoteList";
 import AddNote from "./components/AddNote";
 import EditNote from "./components/EditNote";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import ProtectedRoute from "./components/ProtectedRoute";
-import { AuthProvider } from "./context/AuthContext";
+
+import LoginPage from "./components/LoginPage";
+import RegisterPage from "./components/RegisterPage";
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={
-            <ProtectedRoute>
-              <NoteList />
-            </ProtectedRoute>
-          } />
-          <Route path="add" element={
-            <ProtectedRoute>
-              <AddNote />
-            </ProtectedRoute>
-          } />
-          <Route path="edit/:id" element={
-            <ProtectedRoute>
-              <EditNote />
-            </ProtectedRoute>
-          } />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NoteList/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="add" element={<AddNote/>}/>
+        <Route path="edit/:id" element={<EditNote/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
